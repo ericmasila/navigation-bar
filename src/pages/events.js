3634dc63d53c5cf106a6d01b67992a631b190e09
ericmasila <ericmasila254@gmail.com>
2022-06-21 17:02:20 +0300

    initial commit

---
+++ src/pages/events.js
@@ -0,0 +1,17 @@
+import React from 'react';
+  
+const Events = () => {
+  return (
+    <div
+      styles={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Welcome to GeeksforGeeks Events</h1>
+    </div>
+  );
+};  
+export default Events;
